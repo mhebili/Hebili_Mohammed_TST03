e52d2582905f8bd00ef873bd1d706e91bcc8c4e8
MOHAMMED HEBILI <mohammed.hebili@yahoo.com>
2020-11-18 20:35:44 +0100

    Add project files.

---
+++ Hebili_Mohammed_TST03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hebili_Mohammed_TST03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No. of invoice"); 
+            int invNo = int.Parse(Console.ReadLine()); 
+            int[] invoices = new int[invNo]; 
+            Console.WriteLine("Enter minimum discount amount"); 
+            int discountAmount = int.Parse(Console.ReadLine()); 
+            for (int i = 0; i < invNo; i++) 
+            {
+                Console.WriteLine("Enter the amount of Invoice {0}", i + 1);
+                invoices[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(); 
+            for (int i = 0; i < invNo; i++) 
+            {
+                if (invoices[i] >= discountAmount)
+                    Console.WriteLine("Discount of 5% allowed for invoice {0}", i + 1);
+                else
+                    Console.WriteLine("No Discount for invoice {0}", i + 1);
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Question 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No. of PostalCode and City to enter"); 
+            int recordCount = int.Parse(Console.ReadLine()); 
+            object[] arr = new object[recordCount];
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                Address add = new Address();
+                Console.WriteLine("Enter the PostalCode");
+                add.PostalCode = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the City");
+                add.City = Console.ReadLine();
+                arr[i] = add;
+            }
+            Console.WriteLine(); 
+
+            Console.WriteLine("Enter a postal code to search");
+            int postalCode = int.Parse(Console.ReadLine()); ;
+            bool isFound = false;
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                if (((Address)arr[i]).PostalCode == postalCode)
+                {
+                    Console.WriteLine("Postal Code: {0} City: {1}", ((Address)arr[i]).PostalCode, ((Address)arr[i]).City);
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound)
+                Console.WriteLine("Not Found");
+            Console.ReadLine(); 
+        }
+    }
+    class Address
+    {
+        public int PostalCode { get; set; }
+        public string City { get; set; }
+    }
+    
+}
